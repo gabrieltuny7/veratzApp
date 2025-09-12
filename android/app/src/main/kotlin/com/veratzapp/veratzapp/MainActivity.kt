de8473bdbf6eb247dd55c05247a77ac1123a4c6a
Gabriel Costa Quirino <gbtuny@gmail.com>
2025-09-12 17:37:37 -0300

    Versao 0 - Testando o ambiente de desenvolvimento

---
+++ android/app/src/main/kotlin/com/veratzapp/veratzapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.veratzapp.veratzapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Olá Flutter',
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Olá Flutter')),
+      body: const Center(child: Text('Olá, mundo!')),
+    );
+  }
+}
